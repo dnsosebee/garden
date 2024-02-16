1b85a8f3dae56a54ddbb7b4c43ad38e2194809c0
Daniel Sosebee <dnsosebee@gmail.com>
2024-02-15 22:31:01 -0800

    start work on riddles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,50 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Daniel Sosebee",
+      description: "Daniel Sosebee's digital garden. Posts, journals, and references.",
+      social: {
+        github: "https://github.com/dnsosebee/notes",
+        twitter: "https://twitter.com/dnsosebee",
+        linkedin: "https://www.linkedin.com/in/danielsosebee/",
+      },
+      sidebar: [
+        {
+          label: "Riddles",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "List of All Approved Riddles",
+              link: "/riddles/",
+            },
+            {
+              label: "The Egg Drop Riddle",
+              link: "/riddles/egg-drop/",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+      lastUpdated: true,
+      favicon: "/favicon.ico",
+      customCss: [
+        // Path to your Tailwind base styles:
+        "./src/tailwind.css",
+      ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
